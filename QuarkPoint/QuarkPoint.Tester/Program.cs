a0278d4cc89ee4cad66788a6fcfbd62211703213
vyadzmak <vyadzmak@gmail.com>
2017-10-31 11:41:32 +0300

    commit tester commit

---
+++ QuarkPoint/QuarkPoint.Tester/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuarkPoint.Tester
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+
+        public static MainForm MainForm;
+        [STAThread]
+
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            MainForm = new MainForm();
+            Application.Run(MainForm);
+        }
+    }
+}
